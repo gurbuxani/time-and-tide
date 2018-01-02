aac10616bb93835d498c65a3867f386f019d9c61
Ashwin Gurbuxani <gurbuxani@users.noreply.github.com>
2018-01-02 19:03:08 +0530

    add doubleDigit function

--- sketch.js
+++ sketch.js
@@ -9,27 +9,27 @@ function setup() {
    translate(400, 400);
    rotate(-90);
 
-   let hr = hour();
-   let mn = minute();
-   let sc = second();
+   let currentHour = hour();
+   let currentMinute = minute();
+   let currentSecond = second();
 
 // seconds arc
    strokeWeight(2);
    stroke(255);
    noFill();
-   let secondAngle = map(sc, 0, 60, 0, 360);
+   let secondAngle = map(currentSecond, 0, 60, 0, 360);
    arc(0, 0, 350, 350, 0, secondAngle);
 
 // minutes arc
    strokeWeight(4);
    stroke(255);
-   let minuteAngle = map(mn, 0, 60, 0, 360);
+   let minuteAngle = map(currentMinute, 0, 60, 0, 360);
    arc(0, 0, 280, 280, 0, minuteAngle);
 
 // hours arc
     strokeWeight(8);
     stroke(255);
-    let hourAngle = map(hr % 12, 0, 12, 0, 360);
+    let hourAngle = map(currentHour % 12, 0, 12, 0, 360);
     arc(0, 0, 150, 150, 0, hourAngle);
 
 // center point
@@ -41,13 +41,14 @@ function setup() {
     rotate(90);
     fill(255);
     noStroke();
-    if (hr > 9 && mn > 9 && sc > 9) {
-      text(hr + ' : ' + mn + ' : ' + sc, 5, 400);
-    } else if (hr < 10) {
-      text('0' + hr + ' : ' + mn + ' : ' + sc, 5, 400);
-    } else if (mn < 10) {
-      text(hr + ' : ' + '0' + mn + ' : ' + sc, 5, 400);
-    } else if (sc < 10) {
-      text(hr + ' : ' + mn + ' : ' + '0' + sc, 5, 400);
-    }
+    let time = doubleDigits(currentHour) + ' : ' + doubleDigits(currentMinute) + ' : ' + doubleDigits(currentSecond);
+    timeWidth = textWidth(time);
+    text(time, 0 - timeWidth/2, 300);
+}
+
+// convert time to two digits
+function doubleDigits(n) {
+  if (n < 10){
+  return '0' + n;
+  } else return n;
 }
